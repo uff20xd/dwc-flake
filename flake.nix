68ec266e5033a49cc62841fc2c695cabb7502a7e
uff20xd <uff20xd@proton.me>
2026-03-08 17:56:29 +0100

    k

--- flake.nix
+++ flake.nix
@@ -20,12 +20,50 @@
               hash = "";
             };
             buildInputs = with pkgs; [
-              bmake
               gcc
+              bmake
               pkg-config
-              wayland-scanner 
+              udev 
+              xcb-proto
+              libdrm
+              wayland-protocols
               wayland
+              fontconfig
+              pixman
+              wayland-scanner
+              neuwld
+              neuswc
+              libxkbcommon
+              libdrm
             ];
+          };
+          neuswc = pkgs.stdenv.mkDerivation rec {
+            name = "neuswc";
+            src = pkgs.fetchgit {
+              url = "https://git.sr.ht/~shrub900/neuswc";
+              hash = "sha256-2y7nKZKKWQaxJSuz5ia4VIcR4ibsAt/M6oqDy5jRpg4=";
+            };
+            buildInputs = with pkgs; [
+              gcc
+              bmake
+              pkg-config
+              udev 
+              xcb-proto
+              libdrm
+              wayland-protocols
+              wayland
+              fontconfig
+              pixman
+              wayland-scanner
+              neuwld
+              libxkbcommon
+              libdrm
+            ];
+            installPhase = ''
+            runHook preInstall
+            bmake install PREFIX=$out
+            runHook postInstall
+            '';
           };
           neuwld = pkgs.stdenv.mkDerivation rec {
             name = "neuwld";
