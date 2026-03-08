1e334551faa1351904e033fbf0d6af2884eb0dc9
uff20xd <uff20xd@proton.me>
2026-03-08 16:26:14 +0100

    k

--- flake.nix
+++ flake.nix
@@ -3,15 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neuswc = "github:uff20xd/neuswc-flake";
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, neuswc }: 
     let 
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = pkgsRaw: evaluation: (nixpkgs.lib.genAttrs supportedSystems) (system:  evaluation system pkgsRaw.${system});
     in {
       packages = forAllSystems nixpkgs.legacyPackages (system: pkgs: rec {
-          default = pkgs.hello;
+          default = dwc;
+          dwc = pkgs.stdenv.mkDerivation rec {
+            name = "dwc";
+            src = fetchGit {
+              url = "https://git.sr.ht/~corg/DWC";
+              hash = "";
+            };
+            buildInputs = with pkgs; [
+              bmake
+              gcc
+              pkg-config
+              wayland-scanner 
+              wayland
+              neuswc.default
+            ];
+          };
           });
       devShells = forAllSystems nixpkgs.legacyPackages (system: pkgs: rec {
         default = pkgs.mkShellNoCC rec {
@@ -21,5 +37,5 @@
           ];
         };
       });
-    }
+    };
 }
